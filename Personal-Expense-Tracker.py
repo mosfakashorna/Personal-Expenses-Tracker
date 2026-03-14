1fe919e25c9d0c5298db5abc48c33e2982a03fee
Mosfaka Shorna <mosfaka5678@gmail.com>
2026-03-14 16:17:53 -0400

    Refactor Personal Expense Tracker into a class

--- Personal-Expense-Tracker.py
+++ Personal-Expense-Tracker.py
@@ -1,41 +1,48 @@
-expenses = {}
+class ExpenseTracker:
+    def __init__(self):
+        self.expenses = {}
 
-print("""
+    def add_expense(self, category, amount):
+        if category in self.expenses:
+            self.expenses[category] += amount
+        else:
+            self.expenses[category] = amount
+
+    def show_expenses(self):
+        if len(self.expenses) == 0:
+            print("No expenses has been recorded yet!")
+        else:
+            for category in self.expenses:
+                print(f"{category:<14} = ${self.expenses[category]}")
+            print("-----------------------")
+                      
+            total_spent= 0
+            for amount in self.expenses.values():
+                total_spent += amount
+            print(f"{"Total spent":<14} = ${total_spent}")
+
+tracker = ExpenseTracker()
+
+while True:
+    print("""
 1 Add expense
 2 View total spending by category
 3 Exit
-""")
-
-while True:
-
+    """)
+    
     choice = input("\nChoose option: ")
-    print()
 
     if choice == "1":
         category = input("Enter category: ").capitalize()
         amount = float(input("Enter amount: $"))
-        if category in expenses:
-            expenses[category] += amount
-        else:
-            expenses[category] = amount
-
+        tracker.add_expense(category, amount)
+        
     elif choice == "2":
-        if len(expenses) == 0:
-            print("No expense has redored yet.")
-        else:
-            for category in expenses:
-                print(f"{category:<14} = ${expenses[category]}")
-            print("--------------------------")
-
-            total_spent = 0
-            for amount in expenses.values():
-                total_spent += amount
-
-            print(f"{"Total spent":<14} = ${total_spent}")
+           tracker.show_expenses()
 
     elif choice == "3":
         print("Good bye.")
         break
 
     else:
-        print("I do not understand. \nChoose between (1-4).")
+        print("I do not understand. \nChoose between 1-3.")
